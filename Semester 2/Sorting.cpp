da32ab07a4b6eef674fc76fb990a3070f3900095
Muhammad Ahmed Subhani <ahmadsubhani6741@gmail.com>
2025-11-17 09:13:20 +0500

    Add sorting algorithm implementation in C++

---
+++ Semester 2/Sorting.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int* arr = new int[size];
+
+    cout << "Enter " << size << " elements:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Original Array: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        int minIdx = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[minIdx])
+            {
+                minIdx = j;
+            }
+        }
+
+        int temp = arr[minIdx];
+        arr[minIdx] = arr[i];
+        arr[i] = temp;
+    }
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
